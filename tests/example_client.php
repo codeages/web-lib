66a32796c87bdc521a2244355453b8f31edb82a2
Wellming Li <wellming.li@howzhi.com>
2017-07-03 00:53:25 +0800

    fix test

--- tests/example_client.php
+++ tests/example_client.php
@@ -3,15 +3,14 @@
 require dirname(__DIR__).'/vendor/autoload.php';
 
 use Codeages\RestApiClient\RestApiClient;
-use Codeages\RestApiClient\Specification\JsonHmacSpecification;
-use Codeages\RestApiClient\Specification\SimpleJsonHmacSpecification;
+use Codeages\RestApiClient\Specification\JsonHmacSpecification2;
 
 $config = array(
     'accessKey' => 'key_ok',
     'secretKey' => 'key_ok_secret',
     'endpoint' => 'http://localhost:8000',
 );
-$spec = new SimpleJsonHmacSpecification('sha1');
+$spec = new JsonHmacSpecification2('sha1');
 $client = new RestApiClient($config, $spec);
 $result = $client->get('/');
 
@@ -22,7 +21,7 @@ $config = array(
     'secretKey' => 'key_localhost_secret',
     'endpoint' => 'http://localhost:8000',
 );
-$spec = new SimpleJsonHmacSpecification('sha1');
+$spec = new JsonHmacSpecification2('sha1');
 $client = new RestApiClient($config, $spec);
 $result = $client->get('/');
 
